d7813c3e3e8d62237ddd4e3e5f7c9a788a52e1ac
Mohammad Tayseer <m.tayseer82@gmail.com>
2014-06-14 13:53:01 +0200

    Make the route align itself with the road + using the latest version of leaflet

---
+++ taxi-ride.js
@@ -0,0 +1,104 @@
+var map;
+
+var mapboxLayer = L.tileLayer("https://{s}.tiles.mapbox.com/v3/examples.map-zr0njcqy/{z}/{x}/{y}.png", {
+    attribution: 'Tiles courtesy of <a href="http://www.mapbox.com/" target="_blank">MapBox</a>. Map data (c) <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors, CC-BY-SA.'
+});
+
+map = L.map("map", {
+    zoom: 11,
+    center: new L.LatLng(30.067, 31.264),
+    layers: [mapboxLayer]
+});
+
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+
+var drawControl = new L.Control.Draw({
+    draw: {
+        polyline: {
+            metric: true,
+            shapeOptions: {
+                color: 'green',
+                weight: 4
+            }
+        },
+        circle: false, polygon: false, marker: false, rectangle: false
+    },
+    edit: {
+        featureGroup: drawnItems
+    }
+});
+
+// Change the tooltip text of the control. This should be done before the control is added to the map
+L.drawLocal.draw.toolbar.buttons.polyline = 'Draw the taxi path';
+map.addControl(drawControl);
+
+L.Polyline.prototype.length = function() {
+    var length = 0;
+    for (var i = 0; i < this._latlngs.length - 1; i++) {
+        length += this._latlngs[i].distanceTo(this._latlngs[i+1]);
+    }
+    return length;
+};
+
+function pathCost(lengthInM) {
+    var lengthInKM = lengthInM / 1000;
+    var cost = Math.max(2.5, 2.5 + 1.25 * (lengthInKM - 1));
+    return Math.round(cost * 4) / 4;
+}
+
+// Copied from https://github.com/DennisOSRM/Project-OSRM-Web/blob/master/WebContent/routing/OSRM.RoutingGeometry.js
+function decode(encoded, precision) {
+    precision = Math.pow(10, -precision);
+    var len = encoded.length, index=0, lat=0, lng = 0, array = [];
+    while (index < len) {
+        var b, shift = 0, result = 0;
+        do {
+            b = encoded.charCodeAt(index++) - 63;
+            result |= (b & 0x1f) << shift;
+            shift += 5;
+        } while (b >= 0x20);
+        var dlat = ((result & 1) ? ~(result >> 1) : (result >> 1));
+        lat += dlat;
+        shift = 0;
+        result = 0;
+        do {
+            b = encoded.charCodeAt(index++) - 63;
+            result |= (b & 0x1f) << shift;
+            shift += 5;
+        } while (b >= 0x20);
+        var dlng = ((result & 1) ? ~(result >> 1) : (result >> 1));
+        lng += dlng;
+        //array.push( {lat: lat * precision, lng: lng * precision} );
+        array.push( [lat * precision, lng * precision] );
+    }
+    return array;
+}
+
+map.on('draw:created', function (e) {
+    var type = e.layerType,
+        layer = e.layer;
+
+    if (type === 'polyline') {
+        var url = 'https://router.project-osrm.org/viaroute?z=14&output=json&jsonp=?&instructions=true';
+        for (var i = 0; i < layer._latlngs.length; i++) {
+            var point = layer._latlngs[i];
+            url += '&loc=' + point.lat.toFixed(6) + ',' + point.lng.toFixed(6);
+        }
+        $.ajax({
+            dataType: "json",
+            url: url,
+            cache: true
+        }).done(function(response) {
+            console.log(response);
+            var line = decode(response['route_geometry'], 6);
+            //L.polyline(line).addTo(map);
+            layer.setLatLngs(line).bindLabel("Path length = " + L.GeometryUtil.readableDistance(layer.length(), true) + "<br>" +
+                        "Cost = " + pathCost(layer.length()) + " EGP");
+        });
+
+        
+    }
+
+    drawnItems.addLayer(layer);
+});
